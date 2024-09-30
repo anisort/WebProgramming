d63bb4d2dee0eeb03268db10f63f2aee8a36fca6
anisort <jaki.anisort@gmail.com>
2024-10-01 00:31:49 +0300

    Adding lab6

---
+++ lab6/webpack-project/src/index.js
@@ -0,0 +1,6 @@
+import './scss/styles.scss';
+import './scss/rozklad-styles.scss';
+import './scss/photo-styles.scss';
+import './scss/news-styles.scss';
+// import './scss/index.scss';
+console.log("Hello world!");
---
+++ lab6/webpack-project/webpack.config.js
@@ -0,0 +1,88 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: 'production',
+    entry: {
+        main: path.resolve(__dirname, './src/index.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',   // Загрузка CSS
+                    'sass-loader',  // Компиляция SCSS в CSS
+                ],
+            },
+            {
+                test: /\.css$/,  // Поддержка обычных CSS файлов
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                ],
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif|svg)$/i,  // Поддержка изображений
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/images/[name][ext]',
+                },
+            },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Home',
+            template: path.resolve(__dirname, './src/pages/index.html'),
+            filename: 'index.html',
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Hobby',
+            template: path.resolve(__dirname, './src/pages/about.html'),
+            filename: 'about.html',
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Schedule',
+            template: path.resolve(__dirname, './src/pages/rozklad.html'),
+            filename: 'rozklad.html',
+        }),
+        new HtmlWebpackPlugin({
+            title: 'Gallery',
+            template: path.resolve(__dirname, './src/pages/photo.html'),
+            filename: 'photo.html',
+        }),
+        new HtmlWebpackPlugin({
+            title: 'News',
+            template: path.resolve(__dirname, './src/pages/news.html'),
+            filename: 'news.html',
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, './src/assets/images'),
+                    to: path.resolve(__dirname, './dist/assets/images'),
+                },
+            ],
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+        }),
+    ],
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 8080,
+    },
+};
